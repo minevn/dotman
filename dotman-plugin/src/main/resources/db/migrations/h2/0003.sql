17feec5addd05f30875b9d99205f0dcfa62f01cb
Dương Hồng Ân <themasterclaus@gmail.com>
2024-02-12 18:16:27 +0700

    table creation

---
+++ dotman-plugin/src/main/resources/db/migrations/h2/0003.sql
@@ -0,0 +1,32 @@
+-- Adjust uuid of old tables to varchar(36)
+ALTER TABLE "dotman_napthe_log"
+    ALTER COLUMN "uuid" SET DATA TYPE VARCHAR(36);
+ALTER TABLE "dotman_napthe_log"
+    ALTER COLUMN "uuid" SET NOT NULL;
+
+ALTER TABLE "dotman_player_data"
+    ALTER COLUMN "uuid" SET DATA TYPE VARCHAR(36);
+ALTER TABLE "dotman_player_data"
+    ALTER COLUMN "uuid" SET NOT NULL;
+
+ALTER TABLE "dotman_point_log"
+    ALTER COLUMN "uuid" SET DATA TYPE VARCHAR(36);
+ALTER TABLE "dotman_point_log"
+    ALTER COLUMN "uuid" SET NOT NULL;
+
+-- tao index cho uuid
+create index dotman_napthe_log_uuid_index
+    on "dotman_napthe_log" ("uuid");
+
+create index dotman_point_log_uuid_index
+    on "dotman_point_log" ("uuid");
+
+
+-- Create table to store uuid
+CREATE TABLE "dotman_player_info"
+(
+    "uuid" VARCHAR(36) NOT NULL,
+    "name" VARCHAR(32) NOT NULL,
+    CONSTRAINT "dotman_player_info_pk"
+        PRIMARY KEY ("uuid")
+);
---
+++ dotman-plugin/src/main/resources/db/migrations/mysql/0003.sql
@@ -0,0 +1,25 @@
+-- điều chỉnh uuid các table cũ thành char(36)
+alter table dotman_napthe_log
+    modify uuid char(36) not null;
+
+alter table dotman_player_data
+    modify uuid char(36) not null;
+
+alter table dotman_point_log
+    modify uuid char(36) not null;
+
+-- them index cho uuid
+create index dotman_point_log_uuid_index
+    on dotman_point_log (uuid);
+
+create index dotman_napthe_log_uuid_index
+    on dotman_napthe_log (uuid);
+
+-- tạo bảng lưu uuid
+create table dotman_player_info
+(
+    uuid char(36)    not null,
+    name varchar(32) not null,
+    constraint dotman_player_info_pk
+        primary key (uuid)
+);
