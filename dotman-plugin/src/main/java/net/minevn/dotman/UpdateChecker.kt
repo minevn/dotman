3fbb9dc78b417f2ce71312ce01870b7481c6f226
tranlong5252 <longpham5252@gmail.com>
2024-02-28 21:03:27 +0700

    make update messages configurable

--- dotman-plugin/src/main/java/net/minevn/dotman/UpdateChecker.kt
+++ dotman-plugin/src/main/java/net/minevn/dotman/UpdateChecker.kt
@@ -9,15 +9,19 @@ class UpdateChecker {
     private val url = "https://api.github.com/repos/MineVN/DotMan/releases/latest"
     private var releaseVersion : String = ""
     private val currentVersion = plugin.description.version.trim()
+    private var latestVersion = currentVersion
     private var latest = false
 
     fun init() {
         latest = checkUpdate()
         if (latest) {
-            plugin.logger.info("You are using the latest version of DotMan.")
+            plugin.logger.info(plugin.language.updateLatest)
         } else {
-            plugin.logger.info("There is a new version of DotMan available.")
-            plugin.logger.info("You can download it at $releaseVersion")
+            plugin.logger.info(plugin.language.updateAvailable
+                .replace("%NEW_VERSION%", "")
+                .replace("%CURRENT_VERSION%", currentVersion)
+            )
+            plugin.logger.info(plugin.language.updateAvailableLink.replace("%URL%", releaseVersion))
         }
     }
 
@@ -32,7 +36,7 @@ class UpdateChecker {
     private fun checkUpdate(): Boolean {
         val data = get(url)
         val json = JsonParser().parse(data).asJsonObject
-        val latestVersion = json.get("tag_name").asString
+        latestVersion = json.get("tag_name").asString
         releaseVersion = json.get("html_url").asString
         return latestVersion != currentVersion
     }
