eec7f83fec268b0b5a0dac19337677251a869e7d
tranlong5252 <longpham5252@gmail.com>
2024-03-12 22:07:25 +0700

    compress init

--- dotman-plugin/src/main/java/net/minevn/dotman/UpdateChecker.kt
+++ dotman-plugin/src/main/java/net/minevn/dotman/UpdateChecker.kt
@@ -1,6 +1,5 @@
 package net.minevn.dotman
 
-import net.minevn.dotman.utils.Utils.Companion.info
 import net.minevn.dotman.utils.Utils.Companion.warning
 import net.minevn.libs.bukkit.parseJson
 import net.minevn.libs.get
@@ -8,9 +7,9 @@ import org.bukkit.command.CommandSender
 
 class UpdateChecker {
     companion object {
+        private const val URL = "https://api.github.com/repos/MineVN/DotMan/releases/latest"
         private val plugin = DotMan.instance
         private val language = plugin.language
-        private val url = "https://api.github.com/repos/MineVN/DotMan/releases/latest"
         private var releaseVersion : String = ""
         private val currentVersion = plugin.description.version.trim()
         private var latestVersion = currentVersion
@@ -18,30 +17,26 @@ class UpdateChecker {
 
         fun init() {
             latest = checkUpdate()
-            if (latest) {
-                info(this.language.updateLatest)
-            } else {
-                language.updateAvailable
-                    .replace("%NEW_VERSION%", "")
-                    .replace("%CURRENT_VERSION%", currentVersion)
-                    .let { info(it) }
-                info(this.language.updateAvailableLink.replace("%URL%", releaseVersion))
-            }
+            loginCheckForUpdates(plugin.server.consoleSender)
         }
 
         fun loginCheckForUpdates(receiver: CommandSender) {
-            if (plugin.config.checkUpdate && !latest && receiver.hasPermission("dotman.update")) {
+            if (!plugin.config.checkUpdate || !receiver.hasPermission("dotman.update")) return
+            if (!latest) {
                 language.updateAvailable
                     .replace("%NEW_VERSION%", latestVersion)
                     .replace("%CURRENT_VERSION%", currentVersion)
                     .let { receiver.sendMessage(it) }
                 receiver.sendMessage(language.updateAvailableLink.replace("%URL%", releaseVersion))
             }
+            else {
+                receiver.sendMessage(language.updateLatest)
+            }
         }
 
         private fun checkUpdate(): Boolean {
             try {
-                get(url).parseJson().asJsonObject.let {
+                get(URL).parseJson().asJsonObject.let {
                     latestVersion = it["tag_name"].asString
                     releaseVersion = it["html_url"].asString
                     return latestVersion != currentVersion
