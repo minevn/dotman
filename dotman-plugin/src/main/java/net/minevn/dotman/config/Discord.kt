50b7a8876e843d6e57dbe8acb72cb5302965e94b
Dương Hồng Ân <themasterclaus@gmail.com>
2024-06-03 00:10:04 +0700

    wip discord

--- dotman-plugin/src/main/java/net/minevn/dotman/config/Discord.kt
+++ dotman-plugin/src/main/java/net/minevn/dotman/config/Discord.kt
@@ -9,14 +9,10 @@ class Discord : FileConfig("discord") {
         }
     }
 
-    class DTO(val content: String)
+    data class DTO(val content: String)
 
-    val list: List<WebHook>
-
-    init {
-        list = (config.getList("webhooks") ?: emptyList()).map {
-            it as Map<*, *>
-            WebHook(it["url"] as String, it["content"] as String)
-        }
+    val list: List<WebHook> = (config.getList("webhooks") ?: emptyList()).map {
+        it as Map<*, *>
+        WebHook(it["url"] as String, it["content"] as String)
     }
 }
